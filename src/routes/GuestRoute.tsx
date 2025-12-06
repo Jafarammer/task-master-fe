52c7c1969e58b27bee53d35396a318a959cbc471
Your Name <jafalkaff96@gmail.com>
2025-12-06 13:08:57 +0700

    change config react router v 6

--- src/routes/GuestRoute.tsx
+++ src/routes/GuestRoute.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, replace } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { getToken } from "../utils/auth";
 
 type Props = {
