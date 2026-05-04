784318a6d0ff100e2773e3aaa5739e7735d31390
Your Name <jafalkaff96@gmail.com>
2026-05-04 09:22:28 +0700

    fixing bug first routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { SnackbarAlert } from "./components";
 // layout
 import AuthLayout from "./layouts/AuthLayout";
@@ -21,6 +21,7 @@ function App() {
     <>
       <SnackbarAlert />
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         {/* Auth layout */}
         <Route
           element={
